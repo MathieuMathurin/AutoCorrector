13229bfb5dac1780045365492c7842245db86896
mathfarm <mathurin.mathieu@courrier.uqam.ca>
2016-04-06 08:46:14 -0400

    Fetch de tous les fichiers est fonctionnel.

--- FacebookMessengerExtractor/FacebookMessengerExtractor/app.js
+++ FacebookMessengerExtractor/FacebookMessengerExtractor/app.js
@@ -1,11 +1,19 @@
-﻿//Le accessToken doit etre pris a partir de https://developers.facebook.com/tools/explorer car mon app FB ne peut pas utiliser d'API inferieur a 2.5
-var accessToken = 'CAACEdEose0cBACt2J31UzTj9Fdb52UHQ4rhQhlfonV01MNzUTOZA4M1TgWHkRarYfCcFQcrAhcLKoVCwL4fuWo7ZA4qx27ZCIsDt8TZAPjLlhmoGaBa8TZCZBioBn9dcQM8blL8NQesMB26jsLngJMiI4JJmOhkWwgQlWxyeo3ZALNvwPhWD3zcGePWzGehkRJ8ZCRwBvhshIQZDZD',
-    _ = require('lodash'),
-    request = require('request-promise');
+﻿//Regarde plus bas pour les instruction de fonctionnement
+
+//Configs
+var user = 'Mathieu Mathurin'
+//Le accessToken doit etre pris a partir de https://developers.facebook.com/tools/explorer car mon app FB ne peut pas utiliser d'API inferieur a 2.5
+var accessToken = 'CAACEdEose0cBADXgY2mnKLAUbgVCcnc12FLdZAOO8gOIX3iNSwlsV8XLFfUZC5KTvu7zQi2zxSitaAT6Ff2t6UmM95gbACdGdon8ZCvZCtDKPBsHjWCyAnTKnLK8J3CZB3DCrAMGhAI84hhf0xnabUw6K4EVr5ZAp6tJZCOZBUcqfSZC8fQQ3FNhdF7Xbk3i6Y5DFlbtTuotrFAZDZD';
+
+
+var _ = require('lodash'),
+    request = require('request-promise'),
+    fs = require('fs');
 
 var conversationsId = [];
-var isConversationsFetchComplete = false;
-var isConversationsMessagesComplete = false;
+var conversationsMessages = [];
+var isConversationsIdFetchComplete = false;
+var conversationsFetchStatus = {};
 
 var fetchConversationsId = function(url){
     var req = request(url)
@@ -18,42 +26,65 @@ var fetchConversationsId = function(url){
                 fetchConversationsId(jBody.paging.next);
             }
             else{
-                isDone = true;
+                isConversationsIdFetchComplete = true;
             }
             var tempConv = _.map(data, function (d) { return d.id });
             conversationsId = conversationsId.concat(tempConv);
         })
-        .finally(function () {            
-            if (isConversationsFetchComplete) {
-                fetchAndSaveConversationMessage();
+        .finally(function () {
+        if (isConversationsIdFetchComplete) {
+                writeConversationIds();
+                //Cette ligne servirait si on avait pas de limite de call
+                //fetchAndSaveConversationsMessages();                              
             }
         });
 }
 
-var fetchAndSaveConversationMessage = function() {
-    conversationsId.forEach(function (id) {
-        fetchConversationMessages(id);
+
+//Sert a ecrire les ids dans un fichier quand tous les call sont fait (pas la meilleur facon puisque le code est rendu sync mais tentait pas de le changer
+var writeConversationIds = function (){
+    var ids = _.join(conversationsId, '\n');
+    fs.writeFile('conversationsIds.txt', ids, function (err) {
+        if (!err) {
+            console.log('Done');
+        }
     });
-    //code pour ecrire dans le fichier. J'envisage aussi de le faire faire a chaque conversation dans un append
 }
 
-var fetchConversationMessages = function (id) {
-    var req = request('https://graph.facebook.com/v2.3/' + id + '?access_token=' + accessToken)
-    .then(function (body){
-        var jBody = JSON.parse(body);
-        
-        var data = jBody.comments.data;
-        //console.log(data);
+//Construit l'url pour la premiere page puis lance la recursion
+var fetchAndSaveConversationsMessages = function(id) {
+    var url = 'https://graph.facebook.com/v2.3/' + id + '/comments?access_token=' + accessToken;
+    fetchConversationMessages(url, id);
+}
+
+//Va chercher les message du user et les enregistre
+var fetchConversationMessages = function (url, id) {
+    var req = request(url)
+    .then(function (body) {
+        var jBody = JSON.parse(body);        
+        var data = jBody.data;
+        //console.log(data);        
         if (jBody.paging && jBody.paging.next) {
             //code pour caller le next d'une conversation
+            fetchConversationMessages(jBody.paging.next, id);
         }
-        var temp = _.chain(data).filter(function (d) { return d.from.name == 'Mathieu Mathurin' }).map(function (m) { return m.message }).value();
-        //console.log(temp);
-    })
-    .finally(function () {
-        
-    })        
+        var temp = _.chain(data).filter(function (d) { return d.from.name == user }).map(function (m) { return m.message }).value();
+        conversationsMessages = conversationsMessages.concat(temp);
+        temp = _.join(temp, '\n');
+        fs.appendFile('messages-' + id  + '.txt', temp, 'utf8', function (err) {
+            if (!err) {
+                console.log('Writing to file');
+            }
+        });
+    });        
 };
 
+/* Pour aller cherche tout t'es conversation ID, uncomment la ligne suivante et comment l'autre.
+ * Une fois fait, tu auras tous les ids de tes conversation dans le fichier conversationIds.txt
+ * 
+ * Le truc c'est de partir manuellement la 2e ligne en indiquant le code de conversation. Si c'est des grosses conversations ca va buster car il y a une limite de 200 call / heure.
+ * Tu peux aussi utiliser le lien du access key pour cibler de quelle conversation il s'agit. (Par confidentialite ou simplement parce que tu ne parle pas beaucoup dans cette conversation)
+ * */
+
 //fetchConversationsId('https://graph.facebook.com/v2.3/me/inbox?access_token=' + accessToken + '&debug=all&fields=id&format=json&method=get&pretty=0&suppress_http_code=1');
-fetchConversationMessages('234222416732567');
+fetchAndSaveConversationsMessages(251195684931060);
